6b5d1e29215ec5f161745c7e5f4704b2683b9cb1
Tony Do <85084360+LaansDole@users.noreply.github.com>
2022-12-04 05:43:10 +0000

    Fix the profile button

--- src/ProfileMenu.js
+++ src/ProfileMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import PersonIcon from "@material-ui/icons/Person";
@@ -18,9 +18,9 @@ export default function SimpleMenu() {
 
   return (
     <div>
-       <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+       <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
           <PersonIcon className="header__icon" fontSize="large" />
-        </Button>
+        </IconButton>
       <Menu
         id="simple-menu"
         anchorEl={anchorEl}
