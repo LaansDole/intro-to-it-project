61ce3e27c49d1f9031545f5d7146120c04bc62b9
Dole longAn <dolelongan@Doles-MacBook-Pro.local>
2022-12-31 23:19:45 +0700

    resolve onboarding

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,42 @@
 import React from "react";
 import LandingCover from "../landing/LandingCover";
+import { makeStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        transform: "translateY(-50px)",
+        textAlign: "center",
+        '& > *': {
+        margin: theme.spacing(1),
+        width: '30ch',
+      },
+    },
+}));
 
 const LandingPage = () => {
+    const classes = useStyles();
     return (
         <div>
             <LandingCover />
+            
+            <form 
+            className={classes.root} 
+            noValidate 
+            autoComplete="on">
+                <TextField 
+                id="outlined-basic" 
+                label="username" 
+                variant="outlined"
+                color="secondary" 
+                />
+                <TextField 
+                id="outlined-basic" 
+                label="password"
+                type="password" 
+                variant="outlined" 
+                color="secondary" 
+                />
+            </form>
         </div>
     )
 }
