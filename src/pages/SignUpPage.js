36ec36b4bfb688aec9b22c86f7bc71a9abff0498
Dole longAn <dolelongan@Doles-MacBook-Pro.local>
2023-01-02 12:56:39 +0700

    resolve manifest.json

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -6,7 +6,6 @@ import { IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
 import { Tooltip } from "@material-ui/core";
-import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     upload: {
